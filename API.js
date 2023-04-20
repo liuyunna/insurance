92b7e978c4c7ed8fbda186febb4b23b59ba8f983
liuyunna <33273825+liuyunna@users.noreply.github.com>
2023-04-20 11:01:27 +0800

    Add files via upload

---
+++ API.js
@@ -0,0 +1,162 @@
+import request from '@/commons/http.js' 
+
+
+/* export default { 
+  wechatLogin (params){
+	  console.log('params',params,'POST')
+	  return request('/api.php/user/wechatLogin',params);
+  }
+} */
+
+export function login(data) {
+  return request({url: '/api.php/user/getopenid',method: 'POST', data})
+}
+export function bindusers(data) {
+  return request({url: '/api.php/user/bindusers',method: 'POST', data})
+}
+export function myBind(data) {
+  return request({url: '/api.php/user/myBind',method: 'POST', data})
+}
+export function userAlbum(data) {
+  return request({url: '/api.php/user/userAlbum',method: 'POST', data})
+}
+//图片上传
+export function uploadImg(data) {
+  return request({url: '/api.php/common/upload',method: 'POST', data})
+}
+//打卡
+export function daka(data) {
+  return request({url: '/api.php/user/signUp',method: 'POST', data})
+}
+//长护险选项
+export function options(data) {
+  return request({url: '/api.php/user/options',method: 'POST', data})
+}
+//城市
+export function citys(data) {
+  return request({url: '/api.php/common/getCity',method: 'POST', data})
+}
+//长护险自评
+export function guardianScore(data) {
+  return request({url: '/api.php/user/guardianScore',method: 'POST', data})
+}
+//家属自评打分等级
+export function guardianScoreLevel(data) {
+  return request({url: '/api.php/user/guardianScoreLevel',method: 'POST', data})
+}
+//长护险申请
+export function applyInsurance(data) {
+  return request({url: '/api.php/user/applyInsurance',method: 'POST', data})
+}
+//根据身份证查询用户
+export function getagename(data) {
+  return request({url: '/api.php/user/getagename',method: 'POST', data})
+}
+//长护险评估
+export function insuranceAssessment(data) {
+  return request({url: '/api.php/user/insuranceAssessment',method: 'POST', data})
+}
+//打分
+export function score(data) {
+  return request({url: '/api.php/user/score',method: 'POST', data})
+}
+//专家评估打分
+export function professionalScore(data) {
+  return request({url: '/api.php/user/professionalScore',method: 'POST', data})
+}
+// 更新用户信息
+export function updateUserInfo(data) {
+  return request({url: '/api.php/user/updateUserInfo',method: 'POST', data})
+}
+// 员工巡查打卡
+export function workerCheck(data) {
+  return request({url: '/api.php/user/workerCheck',method: 'POST', data})
+}
+// 员工巡查打卡
+export function addAssessmentVideo(data) {
+  return request({url: '/api.php/user/addAssessmentVideo',method: 'POST', data})
+}
+// 家属评分
+export function addcomment(data) {
+  return request({url: '/api.php/user/addcomment',method: 'POST', data})
+}
+// 机构巡查
+export function patrolFeedback(data) {
+  return request({url: '/api.php/user/patrolFeedback',method: 'POST', data})
+}
+// 居家巡查
+export function homeFeedback(data) {
+  return request({url: '/api.php/user/homeFeedback',method: 'POST', data})
+}
+// 获取护工信息
+export function nurseInfo(data) {
+  return request({url: '/api.php/user/nurseInfo',method: 'POST', data})
+}
+// 编辑护工信息
+export function editNurse(data) {
+  return request({url: '/api.php/user/editNurseInfo',method: 'POST', data})
+}
+// 机构列表
+export function organizationList(data) {
+  return request({url: '/api.php/user/organizationList',method: 'POST', data})
+}
+// 护工-已绑定用户
+export function nurseMyBind(data) {
+  return request({url: '/api.php/user/myBind',method: 'POST', data})
+}
+// 护工-绑定用户
+export function nurseBindusers(data) {
+  return request({url: '/api.php/user/bindusers',method: 'POST', data})
+}
+// 护工-解绑用户
+export function nurseUnbind(data) {
+  return request({url: '/api.php/user/unbind',method: 'POST', data})
+}
+// 护工-开始护理
+export function startServer(data) {
+  return request({url: '/api.php/user/startServer',method: 'POST', data})
+}
+// 护工-结束护理
+export function endServer(data) {
+  return request({url: '/api.php/user/endServer',method: 'POST', data})
+}
+// 护工-结束护理
+export function unfinishedserverid(data) {
+  return request({url: '/api.php/user/unfinishedserverid',method: 'POST', data})
+}
+// 头像上传
+export function uploadAvata(data) {
+  return request({url: '/api.php/user/uploadAvatar',method: 'POST', data})
+}
+// 通知
+export function noticesList(data) {
+  return request({url: '/api.php/user/noticesList',method: 'POST', data})
+}
+// 通知详情
+export function noticesDetail(data) {
+  return request({url: '/api.php/user/noticesDetail',method: 'POST', data})
+}
+// 集中评审宣讲签名
+export function gatherAssessmentSign(data) {
+  return request({url: '/api.php/user/gatherAssessmentSign',method: 'POST', data})
+}
+// 现场评定告知书签名
+export function noticeAssessmentSign(data) {
+  return request({url: '/api.php/user/noticeAssessmentSign',method: 'POST', data})
+}
+// 评估走访
+export function assessmentFeedback(data) {
+  return request({url: '/api.php/user/assessmentFeedback',method: 'POST', data})
+}
+// 获取用户压力数据
+export function getSensorData(data) {
+  return request({url: '/api.php/user/getSensorData',method: 'POST', data})
+}
+// 获取用户压力数据
+export function professionalSort(data) {
+  return request({url: '/api.php/user/professionalSort',method: 'POST', data})
+}
+// 汇报人签名
+export function reportSignPic(data) {
+  return request({url: '/api.php/user/reportSignPic',method: 'POST', data})
+}
---
+++ http.js
@@ -0,0 +1,81 @@
+//把配置项单独处理
+
+
+let server_url='https://jianhu.fmytech.xyz';//请求地址
+uni.setStorageSync("baseUrl", 'https://jianhu.fmytech.xyz');	
+//let token = '5adb273d-a444-40c2-b16f-4d9ff3d43b25';  //凭证
+let token;
+console.log('4',uni.getStorageSync('token'))
+process.env.NODE_ENV === 'development' ? 'https://jianhu.fmytech.xyz/' : 'https://jianhu.fmytech.xyz/' ; //环境配置
+
+function service(options = {}) {
+	uni.showLoading({
+		title: '加载中'
+	});
+	token=uni.getStorageSync('token')
+	console.log('获取token',token)
+       options.url = `${server_url}${options.url}`;
+          //配置请求头
+        options.header = {
+        'content-type': 'application/x-www-form-urlencoded',
+        'token': `${token}` //Bearer 
+
+    };
+
+    return new Promise((resolved, rejected) => {
+                //成功
+        options.success = (res) => {
+			uni.hideLoading()
+            if (Number(res.data.code) == 1) {  //请求成功
+                resolved(res.data);
+            }else if(Number(res.data.code) == 401){
+				uni.showToast({
+				    icon: 'none',   
+				    duration: 3000,
+				    title: `${res.data.msg}`
+				});
+				setTimeout(function(){
+					uni.redirectTo({
+						url: "/pages/login/login"
+					});
+				},2000)
+			}else if(Number(res.data.code) == 0){
+				console.log('请求错误！',res)
+				uni.showToast({
+				    icon: 'none',   
+				    duration: 3000,
+				    title: `${res.data.msg}！`
+				});
+			} else if(Number(res.data.code) == 500){
+				uni.showToast({
+				    icon: 'none',   
+				    duration: 3000,
+				    title: `请求失败，请重试！`
+				});
+			} else {
+				console.log(res)
+                uni.showToast({
+                    icon: 'none',   
+                    duration: 3000,
+                    title: `${res.data.msg}！`
+                });
+                rejected(res.data.msg);//错误
+            }
+			
+        }
+	  //错误
+        options.fail = (err) => {
+            rejected(err); //错误
+			uni.hideLoading()
+			uni.showToast({
+					title: err,//显示的文字
+			        duration: 2000,  //显示多少时间，默认1500毫秒
+			        icon: "success"  //自定义显示的图标，默认成功success，错误error,加载loading，不显示图标是none
+				})
+        }
+        uni.request(options);
+    });
+
+}
+
+export default service;
